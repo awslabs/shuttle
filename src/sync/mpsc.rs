1e08c7dbf749095572b7ea540aa6c79eba5e872b
Rajeev Joshi <jorajeev@amazon.com>
2020-11-08 09:28:47 -0800

    Initial support for mpsc channels.
    Includes support for unbounded and bounded channels (including rendezvous channels).
    Does not yet support nonblocking functions (`try_send` and `try_recv`).  Also does
    not currently implement the `Iterator` trait for receivers.  These will be added
    in a subsequent change.

---
+++ src/sync/mpsc.rs
@@ -0,0 +1,373 @@
+use crate::runtime::execution::ExecutionState;
+use crate::runtime::task::{TaskId, MAX_TASKS};
+use crate::runtime::thread_future;
+use smallvec::SmallVec;
+use std::cell::RefCell;
+use std::fmt::Debug;
+use std::rc::Rc;
+use std::result::Result;
+use std::sync::mpsc;
+use std::sync::Arc;
+use tracing::trace;
+
+pub use mpsc::{RecvError, SendError, TryRecvError};
+
+// TODO
+// * Add support for try_recv() and try_send()
+// * Add support for iter() for receivers
+
+const MAX_INLINE_MESSAGES: usize = 32;
+
+/// Create an unbounded channel
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let channel = Arc::new(Channel::new(None));
+    let sender = Sender {
+        inner: Arc::clone(&channel),
+    };
+    let receiver = Receiver {
+        inner: Arc::clone(&channel),
+    };
+    (sender, receiver)
+}
+
+/// Create a bounded channel
+pub fn sync_channel<T>(bound: usize) -> (SyncSender<T>, Receiver<T>) {
+    let channel = Arc::new(Channel::new(Some(bound)));
+    let sender = SyncSender {
+        inner: Arc::clone(&channel),
+    };
+    let receiver = Receiver {
+        inner: Arc::clone(&channel),
+    };
+    (sender, receiver)
+}
+
+#[derive(Debug)]
+struct Channel<T> {
+    bound: Option<usize>, // None for an unbounded channel, Some(k) for a bounded channel of size k
+    state: Rc<RefCell<ChannelState<T>>>,
+}
+
+// Note: The channels in std::sync::mpsc only support a single Receiver (which cannot be
+// cloned).  The state below admits a more general use case, where multiple Senders
+// and Receivers can share a single channel.
+struct ChannelState<T> {
+    messages: SmallVec<[T; MAX_INLINE_MESSAGES]>,     // messages in the channel
+    known_senders: usize,                             // number of senders referencing this channel
+    known_receivers: usize,                           // number or receivers referencing this channel
+    waiting_senders: SmallVec<[TaskId; MAX_TASKS]>,   // list of currently blocked senders
+    waiting_receivers: SmallVec<[TaskId; MAX_TASKS]>, // list of currently blocked receivers
+}
+
+impl<T> Debug for ChannelState<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Channel {{ ")?;
+        write!(f, "num_messages: {} ", self.messages.len())?;
+        write!(
+            f,
+            "known_senders {} known_receivers {} ",
+            self.known_senders, self.known_receivers
+        )?;
+        write!(f, "waiting_senders: [{:?}] ", self.waiting_senders)?;
+        write!(f, "waiting_receivers: [{:?}] ", self.waiting_receivers)?;
+        write!(f, "}}")
+    }
+}
+
+impl<T> Channel<T> {
+    fn new(bound: Option<usize>) -> Self {
+        Self {
+            bound,
+            state: Rc::new(RefCell::new(ChannelState {
+                messages: SmallVec::new(),
+                known_senders: 1,
+                known_receivers: 1,
+                waiting_senders: SmallVec::new(),
+                waiting_receivers: SmallVec::new(),
+            })),
+        }
+    }
+
+    fn send(&self, message: T) -> Result<(), SendError<T>> {
+        let me = ExecutionState::me();
+        let mut state = self.state.borrow_mut();
+
+        trace!(
+            state = ?state,
+            "sender {:?} starting send on channel {:p}",
+            me,
+            self,
+        );
+        if state.known_receivers == 0 {
+            // No receivers are left, so the channel is disconnected.  Stop and return failure.
+            return Err(SendError(message));
+        }
+
+        let (is_rendezvous, is_full) = if let Some(bound) = self.bound {
+            // For a rendezvous channel (bound = 0), "is_full" holds when there is a message in the channel.
+            // For a non-rendezvous channel (bound > 0), "is_full" holds when the capacity is reached.
+            // We cover both these cases at once using max(bound, 1) below.
+            (bound == 0, state.messages.len() >= std::cmp::max(bound, 1))
+        } else {
+            (false, false)
+        };
+
+        // The sender should block in any of the following situations:
+        //    the channel is full (as defined above)
+        //    there are already waiting senders
+        //    this is a rendezvous channel and there are no waiting receivers
+        let sender_should_block =
+            is_full || !state.waiting_senders.is_empty() || (is_rendezvous && state.waiting_receivers.is_empty());
+
+        state.waiting_senders.push(me);
+        if sender_should_block {
+            trace!(
+                state = ?state,
+                "blocking sender {:?} on channel {:p}",
+                me,
+                self,
+            );
+            ExecutionState::with(|s| s.current_mut().block());
+            drop(state);
+
+            thread_future::switch();
+
+            state = self.state.borrow_mut();
+            trace!(
+                state = ?state,
+                "unblocked sender {:?} on channel {:p}",
+                me,
+                self,
+            );
+
+            // Check again that there are no receivers
+            // We repeat this check because the receivers may have disconnected while the sender was blocked.
+            if state.known_receivers == 0 {
+                // No receivers are left, so the channel is disconnected.  Stop and return failure.
+                return Err(SendError(message));
+            }
+        }
+
+        let head = state.waiting_senders.remove(0);
+        assert_eq!(head, me);
+
+        state.messages.push(message);
+        // The sender has just added a message to the channel, so unblock the first waiting receiver if any
+        if let Some(&tid) = state.waiting_receivers.first() {
+            ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+        }
+        // Check and unblock the next the waiting sender, if eligible
+        if let Some(&tid) = state.waiting_senders.first() {
+            let bound = self.bound.expect("can't have waiting senders on an unbounded channel");
+            if state.messages.len() < bound {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+
+        Ok(())
+    }
+
+    fn recv(&self) -> Result<T, RecvError> {
+        let me = ExecutionState::me();
+        let mut state = self.state.borrow_mut();
+
+        trace!(
+            state = ?state,
+            "starting recv on channel {:p}",
+            self,
+        );
+        // Check if there are any senders left; if not, and the channel is empty, fail with error
+        // (If there are no senders, but the channel is nonempty, the receiver can successfully consume that message.)
+        if state.messages.is_empty() && state.known_senders == 0 {
+            return Err(RecvError);
+        }
+
+        // If this is a rendezvous channel, and the channel is empty, and there are waiting senders,
+        // notify the first waiting sender
+        if self.bound == Some(0) && state.messages.is_empty() {
+            if let Some(&tid) = state.waiting_senders.first() {
+                // maybe_unblock because another receiver may have unblocked the sender already
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+
+        // The receiver should block in any of the following situations:
+        //    the channel is empty
+        //    there are waiting receivers
+        let should_block = state.messages.is_empty() || !state.waiting_receivers.is_empty();
+
+        state.waiting_receivers.push(me);
+        if should_block {
+            trace!(
+                state = ?state,
+                "blocking receiver {:?} on channel {:p}",
+                me,
+                self,
+            );
+            ExecutionState::with(|s| s.current_mut().block());
+            drop(state);
+
+            thread_future::switch();
+
+            state = self.state.borrow_mut();
+            trace!(
+                state = ?state,
+                "unblocked receiver {:?} on channel {:p}",
+                me,
+                self,
+            );
+
+            // Check again if there are any senders left; if not, and the channel is empty, fail with error
+            // (If there are no senders, but the channel is nonempty, the receiver can successfully consume that message.)
+            // We repeat this check because the senders may have disconnected while the receiver was blocked.
+            if state.messages.is_empty() && state.known_senders == 0 {
+                return Err(RecvError);
+            }
+        }
+
+        let head = state.waiting_receivers.remove(0);
+        assert_eq!(head, me);
+
+        let item = state.messages.remove(0);
+        // The receiver has just removed an element from the channel.  Check if any waiting senders
+        // need to be notified.
+        if let Some(&tid) = state.waiting_senders.first() {
+            let bound = self.bound.expect("can't have waiting senders on an unbounded channel");
+            // Unblock the first waiting sender provided one of the following conditions hold:
+            // - this is a non-rendezvous bounded channel (bound > 0)
+            // - this is a rendezvous channel and we have additional waiting receivers
+            if bound > 0 || !state.waiting_receivers.is_empty() {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+        // Check and unblock the next the waiting receiver, if eligible
+        // Note: this is a no-op for mpsc channels, since there can only be one receiver
+        if let Some(&tid) = state.waiting_receivers.first() {
+            if state.messages.len() > 0 {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+
+        Ok(item)
+    }
+}
+
+// Safety: A Channel is never actually passed across true threads, only across continuations. The
+// Rc<RefCell<_>> type therefore can't be preempted mid-bookkeeping-operation.
+// TODO We use this workaround in several places in Shuttle.  Maybe there's a cleaner solution.
+unsafe impl<T> Send for Channel<T> {}
+unsafe impl<T> Sync for Channel<T> {}
+
+/// The receiving half of Rust's [`channel`] (or [`sync_channel`]) type.
+/// This half can only be owned by one thread.
+#[derive(Debug)]
+pub struct Receiver<T> {
+    inner: Arc<Channel<T>>,
+}
+
+impl<T> Receiver<T> {
+    /// Attempts to wait for a value on this receiver, returning an error if the
+    /// corresponding channel has hung up.    
+    pub fn recv(&self) -> Result<T, RecvError> {
+        self.inner.recv()
+    }
+}
+
+impl<T> Drop for Receiver<T> {
+    fn drop(&mut self) {
+        let mut state = self.inner.state.borrow_mut();
+        assert!(state.known_receivers > 0);
+        state.known_receivers -= 1;
+        if state.known_receivers == 0 {
+            // Last receiver was dropped; wake up all senders
+            for &tid in state.waiting_senders.iter() {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+    }
+}
+
+/// The sending-half of Rust's asynchronous [`channel`] type. This half can only be
+/// owned by one thread, but it can be cloned to send to other threads.
+#[derive(Debug)]
+pub struct Sender<T> {
+    inner: Arc<Channel<T>>,
+}
+
+impl<T> Sender<T> {
+    /// Attempts to send a value on this channel, returning it back if it could
+    /// not be sent.    
+    pub fn send(&self, t: T) -> Result<(), SendError<T>> {
+        self.inner.send(t)
+    }
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        let mut state = self.inner.state.borrow_mut();
+        state.known_senders += 1;
+        drop(state);
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut state = self.inner.state.borrow_mut();
+        assert!(state.known_senders > 0);
+        state.known_senders -= 1;
+        if state.known_senders == 0 {
+            // Last sender was dropped; wake up all receivers
+            for &tid in state.waiting_receivers.iter() {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+    }
+}
+
+/// The sending-half of Rust's synchronous [`sync_channel`] type.
+///
+/// Messages can be sent through this channel with [`send`] or [`try_send`].
+///
+/// [`send`] will block if there is no space in the internal buffer.
+#[derive(Debug)]
+pub struct SyncSender<T> {
+    inner: Arc<Channel<T>>,
+}
+
+impl<T> SyncSender<T> {
+    /// Sends a value on this synchronous channel.
+    ///
+    /// This function will *block* until space in the internal buffer becomes
+    /// available or a receiver is available to hand off the message to.    
+    pub fn send(&self, t: T) -> Result<(), SendError<T>> {
+        self.inner.send(t)
+    }
+}
+
+impl<T> Clone for SyncSender<T> {
+    fn clone(&self) -> Self {
+        let mut state = self.inner.state.borrow_mut();
+        state.known_senders += 1;
+        drop(state);
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<T> Drop for SyncSender<T> {
+    fn drop(&mut self) {
+        let mut state = self.inner.state.borrow_mut();
+        assert!(state.known_senders > 0);
+        state.known_senders -= 1;
+        if state.known_senders == 0 {
+            // Last sender was dropped; wake up any receivers
+            for &tid in state.waiting_receivers.iter() {
+                ExecutionState::with(|s| s.get_mut(tid).maybe_unblock());
+            }
+        }
+    }
+}
---
+++ tests/basic/mpsc.rs
@@ -0,0 +1,543 @@
+use shuttle::sync::{channel, sync_channel, RecvError};
+use shuttle::{check_dfs, thread};
+use test_env_log::test;
+
+// The following tests (prefixed with mpsc_loom) are from the
+// loom test suite; see https://github.com/tokio-rs/loom/blob/master/tests/mpsc.rs
+#[test]
+fn mpsc_loom_basic_sequential_usage() {
+    check_dfs(
+        move || {
+            let (s, r) = channel();
+            s.send(5).unwrap();
+            let val = r.recv().unwrap();
+            assert_eq!(val, 5);
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_loom_basic_parallel_usage() {
+    check_dfs(
+        || {
+            let (s, r) = channel();
+            thread::spawn(move || {
+                s.send(5).unwrap();
+            });
+            let val = r.recv().unwrap();
+            assert_eq!(val, 5);
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_loom_commutative_senders() {
+    check_dfs(
+        || {
+            let (s, r) = channel();
+            let s2 = s.clone();
+            thread::spawn(move || {
+                s.send(5).unwrap();
+            });
+            thread::spawn(move || {
+                s2.send(6).unwrap();
+            });
+            let mut val = r.recv().unwrap();
+            val += r.recv().unwrap();
+            assert_eq!(val, 11);
+        },
+        None,
+        None,
+    );
+}
+
+fn ignore_result<A, B>(_: Result<A, B>) {}
+
+#[test]
+#[should_panic]
+fn mpsc_loom_non_commutative_senders1() {
+    check_dfs(
+        || {
+            let (s, r) = channel();
+            let s2 = s.clone();
+            thread::spawn(move || {
+                ignore_result(s.send(5));
+            });
+            thread::spawn(move || {
+                ignore_result(s2.send(6));
+            });
+            let val = r.recv().unwrap();
+            assert_eq!(val, 5);
+            ignore_result(r.recv());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+#[should_panic]
+fn mpsc_loom_non_commutative_senders2() {
+    check_dfs(
+        || {
+            let (s, r) = channel();
+            let s2 = s.clone();
+            thread::spawn(move || {
+                ignore_result(s.send(5));
+            });
+            thread::spawn(move || {
+                ignore_result(s2.send(6));
+            });
+            let val = r.recv().unwrap();
+            assert_eq!(val, 6);
+            ignore_result(r.recv());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_sender_unbounded() {
+    check_dfs(
+        || {
+            let (tx, rx) = channel::<i32>();
+            thread::spawn(move || {
+                drop(tx);
+            });
+            assert!(rx.recv().is_err());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_receiver_unbounded() {
+    check_dfs(
+        || {
+            let (tx, rx) = channel();
+            drop(rx);
+            assert!(tx.send(1).is_err());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_sender_bounded() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<i32>(10);
+            thread::spawn(move || {
+                assert!(rx.recv().is_err());
+            });
+            drop(tx);
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_receiver_bounded() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel(10);
+            drop(rx);
+            assert!(tx.send(1).is_err());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_sender_rendezvous() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<i32>(0);
+            drop(tx);
+            assert!(rx.recv().is_err());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_drop_receiver_rendezvous() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel(0);
+            drop(rx);
+            assert!(tx.send(1).is_err());
+        },
+        None,
+        None,
+    );
+}
+
+// Example taken from the std::sync::mpsc documentation
+// See "buffering behavior" example in
+//   https://doc.rust-lang.org/std/sync/mpsc/struct.Receiver.html
+#[test]
+fn mpsc_buffering_behavior() {
+    check_dfs(
+        || {
+            let (send, recv) = channel();
+            let handle = thread::spawn(move || {
+                send.send(1u8).unwrap();
+                send.send(2).unwrap();
+                send.send(3).unwrap();
+                drop(send);
+            });
+
+            // wait for the thread to join so we ensure the sender is dropped
+            handle.join().unwrap();
+
+            // values sent before the sender disconnects are still available afterwards
+            assert_eq!(Ok(1), recv.recv());
+            assert_eq!(Ok(2), recv.recv());
+            assert_eq!(Ok(3), recv.recv());
+            // but after the values are exhausted, recv() returns an error
+            assert_eq!(Err(RecvError), recv.recv());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_bounded_sum() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<i32>(5);
+            thread::spawn(move || {
+                for _ in 0..5 {
+                    tx.send(1).unwrap();
+                }
+            });
+            let handle = thread::spawn(move || {
+                let mut sum = 0;
+                for _ in 0..5 {
+                    sum += rx.recv().unwrap();
+                }
+                sum
+            });
+            let r = handle.join().unwrap();
+            assert_eq!(r, 5);
+        },
+        None,
+        None,
+    );
+}
+
+// Sending on a bounded channel doesn't block the sender if the channel isn't filled
+#[test]
+fn mpsc_bounded_sender_buffered() {
+    check_dfs(
+        || {
+            let (tx, _rx) = sync_channel::<i32>(10);
+            let handle = thread::spawn(move || {
+                for _ in 0..10 {
+                    tx.send(1).unwrap();
+                }
+                42
+            });
+            let r = handle.join().unwrap();
+            assert_eq!(r, 42);
+        },
+        None,
+        None,
+    );
+}
+
+// Sending on a bounded channel blocks the sender when the channel becomes full
+#[test]
+#[should_panic(expected = "deadlock")]
+fn mpsc_bounded_sender_blocked() {
+    check_dfs(
+        || {
+            let (tx, _rx) = sync_channel::<i32>(10);
+            let handle = thread::spawn(move || {
+                for _ in 0..11 {
+                    tx.send(1).unwrap();
+                }
+                42
+            });
+            let r = handle.join().unwrap();
+            assert_eq!(r, 42);
+        },
+        None,
+        None,
+    );
+}
+
+// The following set of tests (prefixed `mpsc_rendezvous_`) check rendezvous channels.
+
+#[test]
+fn mpsc_rendezvous_channel() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<i32>(0);
+            thread::spawn(move || {
+                // This will wait for the parent thread to start receiving
+                tx.send(53).unwrap();
+            });
+            let v = rx.recv().unwrap();
+            assert_eq!(v, 53);
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+#[should_panic(expected = "deadlock")]
+fn mpsc_rendezvous_sender_block() {
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<i32>(0);
+            tx.send(53).unwrap();
+            rx.recv().unwrap();
+            rx.recv().unwrap();
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_rendezvous_two_threads() {
+    check_dfs(
+        || {
+            let (tx1, rx) = sync_channel::<i32>(0);
+            let tx2 = tx1.clone();
+            thread::spawn(move || {
+                tx1.send(10).unwrap();
+            });
+            thread::spawn(move || {
+                tx2.send(20).unwrap();
+            });
+            let v1 = rx.recv().unwrap();
+            let v2 = rx.recv().unwrap();
+            assert_eq!(v1 + v2, 30);
+        },
+        None,
+        None,
+    );
+}
+
+// An mpsc Receiver is not clone-able and is !Sync, so it can't be shared, but
+// it is Send, so it can be transferred between threads. In this example, we
+// rendezvous two separate threads with the main thread by passing the receiver
+// to the rendezvous channel using a second, bounded channel.
+#[test]
+fn mpsc_rendezvous_transfer_receiver() {
+    check_dfs(
+        || {
+            // First channel is used to send the receiver from one thread to another
+            let (tx1, rx1) = sync_channel(1);
+
+            // Second channel is a rendezvous channel used to synchronize with the main thread
+            let (tx2, rx2) = sync_channel::<i32>(0);
+
+            thread::spawn(move || {
+                let p = rx2.recv().unwrap();
+                assert_eq!(p, 10);
+                // Send the receiver to the 2nd thread
+                tx1.send(rx2).unwrap();
+            });
+
+            let handle = thread::spawn(move || {
+                let rx2 = rx1.recv().unwrap();
+                let q = rx2.recv().unwrap();
+                assert_eq!(q, 20);
+            });
+
+            tx2.send(10).unwrap();
+            tx2.send(20).unwrap();
+
+            // Wait for the 2nd thread to finish
+            handle.join().unwrap();
+        },
+        None,
+        None,
+    );
+}
+
+// From libstd test suite
+#[test]
+fn mpsc_send_from_outside_runtime() {
+    check_dfs(
+        || {
+            let (tx1, rx1) = channel::<()>();
+            let (tx2, rx2) = channel::<i32>();
+            let t1 = thread::spawn(move || {
+                tx1.send(()).unwrap();
+                for _ in 0..7 {
+                    assert_eq!(rx2.recv().unwrap(), 1);
+                }
+            });
+            rx1.recv().unwrap();
+            let t2 = thread::spawn(move || {
+                for _ in 0..7 {
+                    tx2.send(1).unwrap();
+                }
+            });
+            t1.join().ok().expect("thread panicked");
+            t2.join().ok().expect("thread panicked");
+        },
+        None,
+        None,
+    );
+}
+
+// From libstd test suite
+#[test]
+fn mpsc_recv_from_outside_runtime() {
+    check_dfs(
+        || {
+            let (tx, rx) = channel::<i32>();
+            let t = thread::spawn(move || {
+                for _ in 0..10 {
+                    assert_eq!(rx.recv().unwrap(), 1);
+                }
+            });
+            for _ in 0..10 {
+                tx.send(1).unwrap();
+            }
+            t.join().ok().expect("thread panicked");
+        },
+        None,
+        None,
+    );
+}
+
+// From libstd test suite
+// TODO This test actually should not panic, because calling join on a child thread that panics
+// TODO should return an Err with the parameter given to the panic
+// TODO See documentation for `join` in https://doc.rust-lang.org/std/thread/struct.JoinHandle.html
+#[test]
+#[should_panic]
+fn mpsc_oneshot_single_thread_recv_chan_close() {
+    check_dfs(
+        || {
+            // Receiving on a closed chan will panic
+            let res = thread::spawn(move || {
+                let (tx, rx) = channel::<i32>();
+                drop(tx);
+                rx.recv().unwrap();
+            })
+            .join();
+            // What is our res?
+            assert!(res.is_err());
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_many_senders_with_blocking() {
+    const NUM_SENDERS: usize = 4;
+    const CHANNEL_SIZE: usize = 2;
+    const NUM_RECEIVES: usize = NUM_SENDERS - CHANNEL_SIZE;
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<usize>(CHANNEL_SIZE);
+            let senders = (0..NUM_SENDERS)
+                .map(move |i| {
+                    let tx = tx.clone();
+                    thread::spawn(move || {
+                        tx.send(i).unwrap();
+                    })
+                })
+                .collect::<Vec<_>>();
+
+            // Receive enough messages to ensure no sender will block
+            for _ in 0..NUM_RECEIVES {
+                rx.recv().unwrap();
+            }
+            for sender in senders {
+                sender.join().unwrap();
+            }
+        },
+        None,
+        None,
+    );
+}
+
+#[test]
+fn mpsc_many_senders_drop_receiver() {
+    const NUM_SENDERS: usize = 4;
+    const CHANNEL_SIZE: usize = 2;
+    check_dfs(
+        || {
+            let (tx, rx) = sync_channel::<usize>(CHANNEL_SIZE);
+            let senders = (0..NUM_SENDERS)
+                .map(move |i| {
+                    let tx = tx.clone();
+                    thread::spawn(move || {
+                        let _ = tx.send(i);
+                    })
+                })
+                .collect::<Vec<_>>();
+
+            // Drop the receiver; this will unblock any waiting senders
+            drop(rx);
+
+            // Make sure all senders finish
+            for sender in senders {
+                sender.join().unwrap();
+            }
+        },
+        None,
+        None,
+    );
+}
+
+/*
+** TODO: We don't support iter() on receivers yet
+**
+#[test]
+fn test_nested_recv_iter() {
+    check_dfs(|| {
+        let (tx, rx) = sync_channel::<i32>(0);
+        let (total_tx, total_rx) = sync_channel::<i32>(0);
+
+        let _t = thread::spawn(move || {
+            let mut acc = 0;
+            for x in rx.iter() {
+                acc += x;
+            }
+            total_tx.send(acc).unwrap();
+        });
+
+        tx.send(3).unwrap();
+        tx.send(1).unwrap();
+        tx.send(2).unwrap();
+        drop(tx);
+        assert_eq!(total_rx.recv().unwrap(), 6);
+    }, None, None);
+}
+*/
+
+/*
+** TODO: try_recv() not yet supported
+#[test]
+fn mpsc_oneshot_single_thread_peek_close() {
+    check_dfs(|| {
+        let (tx, rx) = channel::<i32>();
+        drop(tx);
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Disconnected));
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Disconnected));
+    }, None, None);
+}
+*/
