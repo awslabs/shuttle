f0bbf997d5c673413d51ef2d8dbda53b9cb01186
James Bornholt <bornholt@amazon.com>
2020-12-15 17:15:54 -0600

    Move some code around for a little more clarity
    Nothing interesting here, just moving a few things around to make
    things clearer (to me).

--- src/runtime/task/mod.rs
+++ src/runtime/task/mod.rs
@@ -26,70 +26,6 @@ use std::rc::Rc;
 // TODO make bigger and configurable
 pub(crate) const MAX_TASKS: usize = 16;
 
-#[derive(PartialEq, Eq, Hash, Clone, Copy, PartialOrd, Ord, Debug)]
-pub struct TaskId(pub(super) usize);
-
-impl From<usize> for TaskId {
-    fn from(id: usize) -> Self {
-        TaskId(id)
-    }
-}
-
-impl From<TaskId> for usize {
-    fn from(tid: TaskId) -> usize {
-        tid.0
-    }
-}
-
-/// A `TaskSet` is a set of `TaskId`s but implemented efficiently as an array of bools.
-// TODO this probably won't work well with large numbers of tasks -- maybe a BitVec?
-#[derive(PartialEq, Eq)]
-pub(crate) struct TaskSet {
-    tasks: [bool; MAX_TASKS],
-}
-
-impl TaskSet {
-    pub fn new() -> Self {
-        Self {
-            tasks: [false; MAX_TASKS],
-        }
-    }
-
-    pub fn contains(&self, tid: TaskId) -> bool {
-        self.tasks[tid.0]
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.tasks.iter().all(|b| !*b)
-    }
-
-    pub fn insert(&mut self, tid: TaskId) {
-        self.tasks[tid.0] = true;
-    }
-
-    pub fn remove(&mut self, tid: TaskId) -> bool {
-        std::mem::replace(&mut self.tasks[tid.0], false)
-    }
-
-    pub fn iter(&self) -> impl Iterator<Item = TaskId> + '_ {
-        self.tasks
-            .iter()
-            .enumerate()
-            .filter(|(_, b)| **b)
-            .map(|(i, _)| TaskId(i))
-    }
-}
-
-impl Debug for TaskSet {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "TaskSet {{ ")?;
-        for t in self.iter() {
-            write!(f, "{} ", t.0)?;
-        }
-        write!(f, "}}")
-    }
-}
-
 /// A `Task` represents a user-level unit of concurrency (currently, that's just a thread). Each
 /// task has an `id` that is unique within the execution, and a `state` reflecting whether the task
 /// is runnable (enabled) or not.
@@ -160,4 +96,68 @@ pub(crate) enum TaskState {
     Runnable,
     Blocked,
     Finished,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy, PartialOrd, Ord, Debug)]
+pub struct TaskId(pub(super) usize);
+
+impl From<usize> for TaskId {
+    fn from(id: usize) -> Self {
+        TaskId(id)
+    }
+}
+
+impl From<TaskId> for usize {
+    fn from(tid: TaskId) -> usize {
+        tid.0
+    }
+}
+
+/// A `TaskSet` is a set of `TaskId`s but implemented efficiently as an array of bools.
+// TODO this probably won't work well with large numbers of tasks -- maybe a BitVec?
+#[derive(PartialEq, Eq)]
+pub(crate) struct TaskSet {
+    tasks: [bool; MAX_TASKS],
+}
+
+impl TaskSet {
+    pub fn new() -> Self {
+        Self {
+            tasks: [false; MAX_TASKS],
+        }
+    }
+
+    pub fn contains(&self, tid: TaskId) -> bool {
+        self.tasks[tid.0]
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.tasks.iter().all(|b| !*b)
+    }
+
+    pub fn insert(&mut self, tid: TaskId) {
+        self.tasks[tid.0] = true;
+    }
+
+    pub fn remove(&mut self, tid: TaskId) -> bool {
+        std::mem::replace(&mut self.tasks[tid.0], false)
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = TaskId> + '_ {
+        self.tasks
+            .iter()
+            .enumerate()
+            .filter(|(_, b)| **b)
+            .map(|(i, _)| TaskId(i))
+    }
+}
+
+impl Debug for TaskSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "TaskSet {{ ")?;
+        for t in self.iter() {
+            write!(f, "{} ", t.0)?;
+        }
+        write!(f, "}}")
+    }
 }
--- tests/asynch/mod.rs
+++ tests/asynch/mod.rs
@@ -1,5 +1,5 @@
 // It's convenient to not specify eval order for `await`s in our tests
 #![allow(clippy::eval_order_dependence)]
 
-mod basic_async_test;
+mod basic;
 mod countdown_timer;
