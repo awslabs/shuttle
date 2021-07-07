1f2f98fb1d26066dd31cf41dfd6720fbea58afbc
James Bornholt <bornholt@amazon.com>
2021-07-07 18:07:02 -0500

    Print a failing schedule even during double panics (#40)
    Double panics are a common problem in concurrent Rust code. For example,
    a thread might panic while holding a lock, and then need to acquire that
    lock again during stack unwinding, but the lock is already poisoned. In
    these cases, Shuttle currently can't print the schedule that lead to the
    original panic, because the double panic aborts the process before our
    `catch_unwind` has a chance to run.
    
    This change adds a new panic hook that gets a chance to run before the
    double panic happens. We use this hook to print the schedule, so that
    even if we double panic in future at least the user gets some output
    they can use to reproduce the problem. There's some trickiness here
    (outlined in a module comment for `failure.rs`) around running different
    panic handlers at different times, which makes the code a little more
    complex than I would have hoped, but it gets the job done.

--- tests/basic/panic.rs
+++ tests/basic/panic.rs
@@ -1,5 +1,7 @@
+use shuttle::scheduler::DfsScheduler;
 use shuttle::sync::{Mutex, RwLock};
-use shuttle::{check_dfs, thread};
+use shuttle::{check_dfs, thread, Config, Runner};
+use std::collections::VecDeque;
 use std::panic::{catch_unwind, AssertUnwindSafe};
 use std::sync::{Arc, PoisonError};
 use test_env_log::test;
@@ -54,4 +56,56 @@ fn rwlock_poison() {
         },
         None,
     )
+}
+
+// This test generates a panic that poisons a lock, and then while unwinding due to that panic, runs
+// a Drop handler that tries to acquire that same lock. That leads to a double panic, which aborts
+// the process. Since this is a common pattern in Rust, we want to check that we at least get a
+// usable schedule printed when this happens.
+#[test]
+#[ignore] // tests a double panic, so we can't enable it by default
+fn max_steps_panic_during_drop() {
+    let config = Config::new();
+    let scheduler = DfsScheduler::new(None, false);
+    let runner = Runner::new(scheduler, config);
+    runner.run(|| {
+        #[derive(Clone)]
+        struct Pool {
+            items: Arc<Mutex<VecDeque<usize>>>,
+        }
+
+        struct PoolItem {
+            pool: Arc<Mutex<VecDeque<usize>>>,
+            item: usize,
+        }
+
+        impl Pool {
+            fn new(length: usize) -> Self {
+                Self {
+                    items: Arc::new(Mutex::new((0..length).collect())),
+                }
+            }
+
+            fn get(&self) -> PoolItem {
+                let mut items = self.items.lock().unwrap();
+                let item = items.pop_front().unwrap();
+                PoolItem {
+                    pool: self.items.clone(),
+                    item,
+                }
+            }
+        }
+
+        impl Drop for PoolItem {
+            fn drop(&mut self) {
+                let mut items = self.pool.lock().unwrap();
+                items.push_back(self.item);
+            }
+        }
+
+        let pool = Pool::new(1);
+
+        let _item1 = pool.get();
+        let _item2 = pool.get();
+    });
 }
