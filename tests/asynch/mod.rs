6a369f960a28b25197a713fc43017dfae09138e8
James Bornholt <bornholt@cs.utexas.edu>
2022-08-23 11:27:37 -0500

    Rust 2021 conversion and dependency bumps (#76)
    Mostly to pick up the new `generator` version, which fixes a bug with
    floating point on aarch64.

--- tests/asynch/mod.rs
+++ tests/asynch/mod.rs
@@ -1,5 +1,5 @@
 // It's convenient to not specify eval order for `await`s in our tests
-#![allow(clippy::eval_order_dependence)]
+#![allow(clippy::mixed_read_write_in_expression)]
 
 mod basic;
 mod channel;
