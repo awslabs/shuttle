5c16fa2675f98544fea468e46c4265c248d95324
Alex Koshelev <koshelev@meta.com>
2022-11-22 14:59:12 -0800

    Update rand module doc to mention new rand version

--- src/rand.rs
+++ src/rand.rs
@@ -1,9 +1,9 @@
-//! Shuttle's implementation of the [`rand`] crate, v0.7.
+//! Shuttle's implementation of the [`rand`] crate, v0.8.
 //!
 //! Shuttle captures and controls the nondeterminism introduced by randomness and allows it to be
 //! replayed deterministically.
 //!
-//! [`rand`]: https://docs.rs/rand/0.7.3/rand/index.html
+//! [`rand`]: https://docs.rs/rand/0.8.5/rand/index.html
 
 /// Random number generators and adapters
 pub mod rngs {
