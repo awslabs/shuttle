a8e1042a5a0e5c299c72cf6f5f1a88888341c489
FinnitoProductions <20FinnF@students.harker.org>
2023-01-24 00:36:51 -0600

    Make code compile with stricter linting added back

--- src/sync/selector.rs
+++ src/sync/selector.rs
@@ -4,15 +4,19 @@ use crate::{sync::mpsc::Receiver, runtime::execution::ExecutionState};
 use core::fmt::Debug;
 use crate::runtime::task::TaskId;
 
+/// Any object which is selectable -- typically used for a receiver.
 pub trait Selectable {
+    /// Attempts to select from the selectable, returning true if anything is present and false otherwise.
     fn try_select(&self) -> bool;
+    /// Adds a queued receiver to the selectable (used when the receiver is about to block).
     fn add_waiting_receiver(&mut self, task: TaskId);
+    /// Removes all instances of a queued receiver from the selectable (used after the receiver has been unblocked).
     fn delete_waiting_receiver(&mut self, task: TaskId);
 }
 
-impl Debug for dyn Selectable {
+impl<'a> Debug for dyn Selectable + 'a {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "Series{{}}")
+        write!(f, "Selectable")
     }
 }
 
@@ -50,26 +54,30 @@ fn select(handles: &mut [(&mut dyn Selectable, usize, *const u8)]) -> usize {
     }
 }
 
-// A selector.
-// #[derive(Debug)]
+/// A selector.
+#[derive(Debug)]
 pub struct Select<'a> {
     handles: Vec<(&'a mut dyn Selectable, usize, *const u8)>,
 }
 
 impl<'a> Select<'a> {
+    /// Creates a new instance of the selector with no selectables.
     pub fn new() -> Self {
         Self { handles: Vec::new() }
     }
 
+    /// Adds a new receiving selectable which the selector will wait on.
     pub fn recv<T>(&mut self, r: &'a mut Receiver<T>) {
         let ptr = r as *const Receiver<_> as *const u8;
         self.handles.push((r, self.handles.len(), ptr))
     }
 
+    /// Attempts to receive from one of the added selectables, returning the index of the given channel if possible.
     pub fn try_select(&mut self) -> Option<usize> {
         try_select(&mut self.handles).map(|(idx, _)| idx)
     }
 
+    /// Blocks until a value can be retrieved from one of the given selectables.
     pub fn select(&mut self) -> usize {
         select(&mut self.handles)
     }
