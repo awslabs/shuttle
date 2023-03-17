efae0595b9c389df714344cedaea40f36d8cbf55
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2023-03-17 14:37:23 +0100

    Test all the panic cases

--- tests/basic/uncontrolled_randomness.rs
+++ tests/basic/uncontrolled_randomness.rs
@@ -1,3 +1,4 @@
+use rand::{rngs::StdRng, SeedableRng};
 use shuttle::rand::Rng;
 use shuttle::scheduler::{DfsScheduler, Scheduler};
 use shuttle::sync::{Arc, Mutex};
@@ -42,11 +43,7 @@ fn randomly_acquire_lock<F: (Fn() -> Box<dyn rand::RngCore>) + Send + Sync>(thre
     threads.into_iter().for_each(|t| t.join().expect("Failed"));
 }
 
-fn spawn_random_amount_of_threads<F: (Fn() -> Box<dyn rand::RngCore>) + Send + Sync>(
-    thread_rng: &'static F,
-    max_threads: u64,
-) {
-    let num_threads: u64 = thread_rng().gen::<u64>() % max_threads;
+fn have_n_threads_acquire_mutex(num_threads: u64) {
     let lock = Arc::new(Mutex::new(0u64));
     let threads: Vec<_> = (0..num_threads)
         .map(|_| {
@@ -60,9 +57,37 @@ fn spawn_random_amount_of_threads<F: (Fn() -> Box<dyn rand::RngCore>) + Send + S
         .collect();
 
     threads.into_iter().for_each(|t| t.join().expect("Failed"));
+}
 
-    let num = lock.lock().unwrap();
-    assert!(*num == num_threads);
+fn spawn_random_amount_of_threads<F: (Fn() -> Box<dyn rand::RngCore>) + Send + Sync>(
+    thread_rng: &'static F,
+    max_threads: u64,
+) {
+    let num_threads: u64 = thread_rng().gen::<u64>() % max_threads;
+    have_n_threads_acquire_mutex(num_threads);
+}
+
+fn spawn_random_amount_of_threads_mutex_rng(rng: &Mutex<StdRng>, max_threads: u64) {
+    let num_threads = rng.lock().unwrap().gen::<u64>() % max_threads;
+    have_n_threads_acquire_mutex(num_threads);
+}
+
+fn have_n_threads_yield(num_threads: u64) {
+    let threads: Vec<_> = (0..num_threads)
+        .map(|_| {
+            thread::spawn(move || {
+                thread::yield_now();
+            })
+        })
+        .collect();
+
+    threads.into_iter().for_each(|t| t.join().expect("Failed"));
+}
+
+fn make_random_numbers() {
+    for _ in 0..10 {
+        shuttle::rand::thread_rng().gen::<u64>();
+    }
 }
 
 #[test]
@@ -111,6 +136,96 @@ fn spawn_random_amount_of_threads_dfs_regular_rand() {
     let scheduler = DfsScheduler::new(None, true);
     check_uncontrolled_randomness_custom_scheduler_and_config(
         || spawn_random_amount_of_threads(&|| Box::new(rand::thread_rng()), 10),
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: current execution should have ended"]
+fn panic_should_have_ended() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || spawn_random_amount_of_threads_mutex_rng(&rng, 2),
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: current execution ended earlier than expected"]
+fn panic_ended_earlier() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || spawn_random_amount_of_threads_mutex_rng(&rng, 3),
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: set of runnable tasks is different than expected"]
+fn panic_set_of_runnable() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || spawn_random_amount_of_threads_mutex_rng(&rng, 15),
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: next step was context switch, but recording expected random number generation"]
+fn panic_context_switch_when_expecting_rng() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    let modulo = 3;
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || {
+            let test = rng.lock().unwrap().gen::<u64>() % modulo == 0;
+            if test {
+                have_n_threads_acquire_mutex(10);
+            } else {
+                make_random_numbers();
+            }
+        },
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: next step was random number generation, but recording expected context switch"]
+fn panic_rng_when_expecting_context_switch() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    let modulo = 5;
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || {
+            let test = rng.lock().unwrap().gen::<u64>() % modulo == 0;
+            if test {
+                have_n_threads_acquire_mutex(10);
+            } else {
+                make_random_numbers();
+            }
+        },
+        scheduler,
+    );
+}
+
+#[test]
+#[should_panic = "possible nondeterminism: `next_task` was called with `is_yielding`"]
+fn panic_is_yielding() {
+    let scheduler = DfsScheduler::new(None, true);
+    let rng = Mutex::new(StdRng::seed_from_u64(123));
+    let modulo = 5;
+    check_uncontrolled_randomness_custom_scheduler_and_config(
+        move || {
+            let test = rng.lock().unwrap().gen::<u64>() % modulo == 0;
+            if test {
+                have_n_threads_acquire_mutex(10);
+            } else {
+                have_n_threads_yield(10);
+            }
+        },
         scheduler,
     );
 }
