b85e5ab7bceff1d3f240e0e1d2a2c790843649b9
Sarek Høverstad Skotåm <sarek.skotam@gmail.com>
2023-03-30 02:22:23 +0100

    Fix tracing spans

--- src/runtime/execution.rs
+++ src/runtime/execution.rs
@@ -10,11 +10,11 @@ use scoped_tls::scoped_thread_local;
 use smallvec::SmallVec;
 use std::any::Any;
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::future::Future;
 use std::panic;
 use std::rc::Rc;
-use tracing::span::Entered;
-use tracing::{span, trace, Level, Span};
+use tracing::trace;
 
 // We use this scoped TLS to smuggle the ExecutionState, which is not 'static, across tasks that
 // need access to it (to spawn new tasks, interrogate task status, etc).
@@ -192,10 +192,7 @@ pub(crate) struct ExecutionState {
     scheduler: Rc<RefCell<dyn Scheduler>>,
     current_schedule: Schedule,
 
-    // For `tracing`, we track the current task's Span here and manage it in `schedule_next_task`.
-    // Drop order is significant here; see the unsafe code in `schedule_next_task` for why.
-    current_span_entered: Option<Entered<'static>>,
-    current_span: Span,
+    task_id_to_span: HashMap<TaskId, tracing::Span>,
 
     #[cfg(debug_assertions)]
     has_cleaned_up: bool,
@@ -234,8 +231,7 @@ impl ExecutionState {
             storage: StorageMap::new(),
             scheduler,
             current_schedule: initial_schedule,
-            current_span_entered: None,
-            current_span: Span::none(),
+            task_id_to_span: HashMap::new(),
             #[cfg(debug_assertions)]
             has_cleaned_up: false,
         }
@@ -552,28 +548,29 @@ impl ExecutionState {
     /// Set the next task as the current task, and update our tracing span
     fn advance_to_next_task(&mut self) {
         debug_assert_ne!(self.next_task, ScheduledTask::None);
+        let previous_task = self.current_task;
         self.current_task = self.next_task.take();
         if let ScheduledTask::Some(tid) = self.current_task {
             self.current_schedule.push_task(tid);
-        }
+            let span = tracing::Span::current();
+            if let Some(span_id) = span.id() {
+                if let ScheduledTask::Some(previous) = previous_task {
+                    self.task_id_to_span.insert(previous, span);
+                    tracing::dispatcher::get_default(|subscriber| {
+                        subscriber.exit(&span_id);
+                    });
+                }
+            }
 
-        // Safety: Unfortunately `ExecutionState` is a static, but `Entered<'a>` is tied to the
-        // lifetime 'a of its corresponding Span, so we can't stash the `Entered` into
-        // `self.current_span_entered` directly. Instead, we transmute `Entered<'a>` into
-        // `Entered<'static>`. We make sure that it can never outlive 'a by dropping
-        // `self.current_span_entered` before dropping the `self.current_span` it points to.
-        self.current_span_entered.take();
-        if let ScheduledTask::Some(tid) = self.current_task {
-            self.current_span = span!(Level::INFO, "step", i = self.current_schedule.len() - 1, task = tid.0);
-            self.current_span_entered = Some(unsafe { extend_span_entered_lt(self.current_span.enter()) });
+            if let Some(span) = self.task_id_to_span.get(&tid) {
+                if let Some(span_id) = span.id() {
+                    tracing::dispatcher::get_default(|subscriber| {
+                        subscriber.enter(&span_id);
+                    });
+                }
+            }
         }
     }
-}
-
-// Safety: see the use in `advance_to_next_task` above. We lift this out of that function so we can
-// give fixed concrete types for the transmute.
-unsafe fn extend_span_entered_lt(entered: Entered) -> Entered<'static> {
-    std::mem::transmute(entered)
 }
 
 #[cfg(debug_assertions)]
