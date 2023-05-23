53cd3221146a172755adb6cf94d3c3d54a88f76a
Bernhard Kragl <kraglb@amazon.com>
2023-05-23 20:03:52 +0200

    Bump `futures` dependency (#107)
    We have a test that relies on `FuturesUnordered` implementing `IntoIter`,
    but this is only the case since 0.3.15.
    
    https://github.com/rust-lang/futures-rs/blob/master/CHANGELOG.md#0315---2021-05-11

--- tests/future/streams.rs
+++ tests/future/streams.rs
@@ -1,50 +1,57 @@
 use futures::{future::join_all, stream::FuturesUnordered, StreamExt};
 use shuttle::{
     check_dfs,
-    future::{block_on, spawn},
+    future::{block_on, spawn, JoinHandle},
 };
 
-fn futures_unordered_collect() {
-    block_on(async {
-        let tasks = (0..2).map(|_| spawn(async move {})).collect::<FuturesUnordered<_>>();
-
-        let _ = tasks.collect::<Vec<_>>().await;
-    });
+fn empty_tasks() -> FuturesUnordered<JoinHandle<()>> {
+    (0..2).map(|_| spawn(async move {})).collect()
 }
 
 #[test]
 fn collect_empty_tasks() {
-    check_dfs(futures_unordered_collect, None);
-}
+    check_dfs(
+        || {
+            block_on(async {
+                let tasks = empty_tasks();
 
-fn futures_unordered_next() {
-    block_on(async {
-        let mut tasks = (0..2).map(|_| spawn(async move {})).collect::<FuturesUnordered<_>>();
-
-        while let Some(result) = tasks.next().await {
-            result.unwrap();
-        }
-    });
+                let _ = tasks.collect::<Vec<_>>().await;
+            })
+        },
+        None,
+    );
 }
 
 #[test]
 fn next_empty_tasks() {
-    check_dfs(futures_unordered_next, None);
-}
+    check_dfs(
+        || {
+            block_on(async {
+                let mut tasks = empty_tasks();
 
-fn futures_join_all() {
-    block_on(async {
-        let tasks = (0..2).map(|_| spawn(async move {})).collect::<FuturesUnordered<_>>();
-
-        join_all(tasks)
-            .await
-            .into_iter()
-            .collect::<Result<Vec<_>, _>>()
-            .unwrap();
-    });
+                while let Some(result) = tasks.next().await {
+                    result.unwrap();
+                }
+            })
+        },
+        None,
+    );
 }
 
 #[test]
 fn join_all_empty_tasks() {
-    check_dfs(futures_join_all, None);
+    check_dfs(
+        || {
+            block_on(async {
+                let tasks = empty_tasks();
+
+                join_all(tasks)
+                    .await
+                    .into_iter()
+                    .collect::<Result<Vec<_>, _>>()
+                    .unwrap();
+            })
+        },
+        None,
+    );
 }
