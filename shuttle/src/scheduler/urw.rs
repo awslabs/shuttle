af8b1f47e64291960c6e4a83ca9d3aa8e0de0e27
Dylan Wolff <wolffdy@amazon.com>
2025-09-11 00:52:01 +0000

    CR: refactoring to use explicit state enum + transitions

--- shuttle/src/scheduler/urw.rs
+++ shuttle/src/scheduler/urw.rs
@@ -12,10 +12,9 @@ use tracing::{trace, warn};
 type SignatureHash = u64;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-#[allow(unused)]
-enum URWState {
+enum UrwSchedulerState {
+    PreEstimation,
     Estimating,
-    EstimatedNotInitialized,
     Initialized,
 }
 
@@ -45,8 +44,7 @@ pub struct UrwRandomScheduler {
     /// Indicates a parent-child relation between the task with signature .0 and .1
     signature_parents: Vec<(SignatureHash, SignatureHash)>,
     /// The current state of the scheduler
-    #[allow(unused)]
-    state: URWState,
+    state: UrwSchedulerState,
 }
 
 impl UrwRandomScheduler {
@@ -85,13 +83,92 @@ impl UrwRandomScheduler {
             signature_event_counts: HashMap::new(),
             min_event_count: usize::MAX,
             signature_parents: Vec::new(),
-            state: URWState::Estimating,
+            state: UrwSchedulerState::PreEstimation,
         }
     }
 
-    #[inline]
-    fn events_counted_but_not_initialized(&self) -> bool {
-        self.task_event_counts.is_none() && !self.signature_event_counts.is_empty()
+    fn initialize_estimates_from_observed_counts(&mut self) {
+        // If we have never initialized the event counts before, we need to aggregate the count estimates
+        // from each child task to its parents
+        trace!("Finished estimation of event counts for URW");
+        trace!(
+            "Estimated event counts for URW (pre-parent subsumption): {:?}",
+            self.signature_event_counts
+        );
+
+        // The implemenation of URW currently assumes that each task has a unique signature in a single execution.
+        // Thus the number of spawns events should equal the number of unique child signatures
+        debug_assert!(
+            self.signature_event_counts
+                .keys()
+                .cloned()
+                .collect::<HashSet<_>>()
+                .len()
+                == self.signature_event_counts.len()
+        );
+
+        // Iterating in reverse spawn order to ensure counts are propagated to grandparents correctly
+        for (parent_sig, child_sig) in self.signature_parents.iter().rev() {
+            let child_ct = *self.signature_event_counts.get(child_sig).unwrap();
+            self.signature_event_counts
+                .entry(*parent_sig)
+                .and_modify(|parent_ct| *parent_ct += child_ct);
+        }
+
+        self.min_event_count = *self.signature_event_counts.values().min().unwrap();
+
+        trace!(
+            "Estimated event counts for URW (post-parent subsumption): {:?}",
+            self.signature_event_counts
+        );
+
+        self.task_event_counts = Some(Vec::new());
+        self.state = UrwSchedulerState::Initialized;
+    }
+
+    fn next_task_urw(&mut self, runnable: &[&Task]) -> Option<TaskId> {
+        let task_event_counts = self.task_event_counts.as_mut().unwrap();
+
+        // We need to loop over the tasks to identify if there has been a `spawn` event
+        // In the future, if we embed a `next_event` enum field into the Task struct, this code can be
+        // simplified to a single conditional
+        for t in runnable {
+            let tid: usize = get_tid(t);
+            if tid == task_event_counts.len() {
+                // Spawn: should remove child events from the parent and map remaining events to the correct task id
+                let child_events = *self
+                    .signature_event_counts
+                    .get(&t.signature.signature_hash())
+                    .unwrap_or_else(|| {
+                        // TODO: we can probably handle unseen tasks less naively than estimating the min event count
+                        warn!(
+                            "No event count for spawn of task with signature {}",
+                            t.signature.signature_hash()
+                        );
+                        &self.min_event_count
+                    });
+                task_event_counts.push(child_events);
+                if let Some(ptid) = t.parent_task_id() {
+                    let ptid: usize = ptid.into();
+                    task_event_counts[ptid] = task_event_counts[ptid].saturating_sub(child_events).max(1);
+                }
+            } else if tid > task_event_counts.len() {
+                panic!("TID's expected to be spawned in ascending order in increments of 1");
+            }
+            // Any runnable task must have at least one remaining event
+            // This is ensured by using `saturating_sub` when decrementing the event counts
+            assert!(task_event_counts[tid] >= 1);
+        }
+
+        let next_tid = runnable
+            .choose_weighted(&mut self.rng, |t| task_event_counts[get_tid(t)])
+            .unwrap()
+            .id();
+        let next_tid_usize: usize = next_tid.into();
+        task_event_counts[next_tid_usize] = task_event_counts[next_tid_usize].saturating_sub(1).max(1);
+
+        trace!("URW remaining event counts: {:?}", task_event_counts);
+        Some(next_tid)
     }
 }
 
@@ -101,116 +178,44 @@ impl Scheduler for UrwRandomScheduler {
             self.signature_event_counts.clear();
             self.signature_parents.clear();
             self.task_event_counts = None;
-            None
-        } else {
-            if self.events_counted_but_not_initialized() {
-                // If we have never initialized the event counts before, we need to aggregate the count estimates
-                // from each child task to its parents
-                trace!("Finished estimation of event counts for URW");
-                trace!(
-                    "Estimated event counts for URW (pre-parent subsumption): {:?}",
-                    self.signature_event_counts
-                );
-
-                // The implemenation of URW currently assumes that each task has a unique signature in a single execution.
-                // Thus the number of spawns events should equal the number of unique child signatures
-                debug_assert!(
-                    self.signature_event_counts
-                        .keys()
-                        .cloned()
-                        .collect::<HashSet<_>>()
-                        .len()
-                        == self.signature_event_counts.len()
-                );
-
-                // Iterating in reverse spawn order to ensure counts are propagated to grandparents correctly
-                for (parent_sig, child_sig) in self.signature_parents.iter().rev() {
-                    let child_ct = *self.signature_event_counts.get(child_sig).unwrap();
-                    self.signature_event_counts
-                        .entry(*parent_sig)
-                        .and_modify(|parent_ct| *parent_ct += child_ct);
-                }
-
-                self.min_event_count = *self.signature_event_counts.values().min().unwrap();
-
-                trace!(
-                    "Estimated event counts for URW (post-parent subsumption): {:?}",
-                    self.signature_event_counts
-                );
-
-                self.task_event_counts = Some(Vec::new());
-            } else if let Some(ec) = self.task_event_counts.as_mut() {
-                // Clear any remaining event counts for the next iteration. Counts are re-initialized
-                // on `spawn` in the next iteration
-                ec.clear();
-            }
-            self.iterations += 1;
-            let seed = self.data_source.reinitialize();
-            self.rng = Pcg64Mcg::seed_from_u64(seed);
-            Some(Schedule::new(seed))
+            return None;
         }
+
+        match self.state {
+            UrwSchedulerState::PreEstimation => self.state = UrwSchedulerState::Estimating,
+            UrwSchedulerState::Estimating => self.initialize_estimates_from_observed_counts(),
+            // Clear any remaining event counts for the next iteration. Counts are re-initialized
+            // on `spawn` in the next iteration
+            UrwSchedulerState::Initialized => self.task_event_counts.as_mut().unwrap().clear(),
+        }
+
+        self.iterations += 1;
+        let seed = self.data_source.reinitialize();
+        self.rng = Pcg64Mcg::seed_from_u64(seed);
+        Some(Schedule::new(seed))
     }
 
     fn next_task(&mut self, runnable: &[&Task], _current: Option<TaskId>, _is_yielding: bool) -> Option<TaskId> {
-        if let Some(task_event_counts) = self.task_event_counts.as_mut() {
-            // If the event counts have been estimated...
+        match self.state {
+            UrwSchedulerState::PreEstimation => unreachable!(),
+            UrwSchedulerState::Estimating => {
+                // Delegate scheduling to vanilla RW when estimating counts
+                let t = runnable.choose(&mut self.rng).unwrap();
 
-            // We need to loop over the tasks to identify if there has been a `spawn` event
-            // In the future, if we embed a `next_event` enum field into the Task struct, this code can be
-            // simplified to a single conditional
-            for t in runnable {
-                let tid: usize = get_tid(t);
-                if tid == task_event_counts.len() {
-                    // Spawn: should remove child events from the parent and map remaining events to the correct task id
-                    let child_events = *self
-                        .signature_event_counts
-                        .get(&t.signature.signature_hash())
-                        .unwrap_or_else(|| {
-                            // TODO: we can probably handle unseen tasks less naively than estimating the min event count
-                            warn!(
-                                "No event count for spawn of task with signature {}",
-                                t.signature.signature_hash()
-                            );
-                            &self.min_event_count
-                        });
-                    task_event_counts.push(child_events);
-                    if let Some(ptid) = t.parent_task_id() {
-                        let ptid: usize = ptid.into();
-                        task_event_counts[ptid] = task_event_counts[ptid].saturating_sub(child_events).max(1);
-                    }
-                } else if tid > task_event_counts.len() {
-                    panic!("TID's expected to be spawned in ascending order in increments of 1");
-                }
-                // Any runnable task must have at least one remaining event
-                // This is ensured by using `saturating_sub` when decrementing the event counts
-                assert!(task_event_counts[tid] >= 1);
+                // If we don't have event counts yet, use the current run to estimate event counts (1-shot)
+                self.signature_event_counts
+                    .entry(t.signature.signature_hash())
+                    .and_modify(|c| *c += 1)
+                    .or_insert_with(|| {
+                        // Spawn
+                        self.signature_parents
+                            .push((t.signature.parent_signature_hash(), t.signature.signature_hash()));
+                        1
+                    });
+
+                Some(t.id())
             }
-
-            let next_tid = runnable
-                .choose_weighted(&mut self.rng, |t| task_event_counts[get_tid(t)])
-                .unwrap()
-                .id();
-            let next_tid_usize: usize = next_tid.into();
-            task_event_counts[next_tid_usize] = task_event_counts[next_tid_usize].saturating_sub(1).max(1);
-
-            trace!("URW remaining event counts: {:?}", task_event_counts);
-            Some(next_tid)
-        } else {
-            // Delegate scheduling to vanilla RW when estimating counts
-            let t = runnable.choose(&mut self.rng).unwrap();
-
-            // If we don't have event counts yet, use the current run to estimate event counts (1-shot)
-            self.signature_event_counts
-                .entry(t.signature.signature_hash())
-                .and_modify(|c| *c += 1)
-                .or_insert_with(|| {
-                    // Spawn
-                    self.signature_parents
-                        .push((t.signature.parent_signature_hash(), t.signature.signature_hash()));
-                    1
-                });
-
-            Some(t.id())
+            UrwSchedulerState::Initialized => self.next_task_urw(runnable),
         }
     }
 
