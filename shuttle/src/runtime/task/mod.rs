8ec3c44c412e7f72dbb87c78a95981182866c3b6
Dylan J. Wolff <wolffdy0@gmail.com>
2025-10-23 15:00:58 -0700

    Generators --> Corosensei (#204)
    * Generators --> Corosensei
    
    * CR: cleanup
    
    * CR: update safety comment; reusable includes initialized
    
    ---------
    
    Co-authored-by: Dylan Wolff <wolffdy@amazon.com>

--- shuttle/src/runtime/task/mod.rs
+++ shuttle/src/runtime/task/mod.rs
@@ -5,10 +5,13 @@ use crate::runtime::storage::{AlreadyDestructedError, StorageKey, StorageMap};
 use crate::runtime::task::clock::VectorClock;
 use crate::runtime::task::labels::Labels;
 use crate::runtime::thread;
-use crate::runtime::thread::continuation::{ContinuationPool, PooledContinuation};
+use crate::runtime::thread::continuation::{
+    ContinuationInput, ContinuationOutput, ContinuationPool, PooledContinuation,
+};
 use crate::sync::{ResourceSignature, ResourceType};
 use crate::thread::LocalKey;
 use bitvec::prelude::*;
+use corosensei::Yielder;
 use std::any::Any;
 use std::backtrace::Backtrace;
 use std::cell::RefCell;
@@ -263,6 +266,7 @@ pub struct Task {
     park_state: ParkState,
 
     pub(super) continuation: Rc<RefCell<PooledContinuation>>,
+    pub(super) yielder: *const Yielder<ContinuationInput, ContinuationOutput>,
 
     pub(crate) clock: VectorClock,
 
@@ -326,6 +330,7 @@ impl Task {
         assert!(id.0 < clock.time.len());
         let mut continuation = ContinuationPool::acquire(stack_size);
         continuation.initialize(f);
+        let yielder = continuation.yielder;
         let waker = make_waker(id);
         let continuation = Rc::new(RefCell::new(continuation));
 
@@ -341,6 +346,7 @@ impl Task {
             parent_task_id,
             state: TaskState::Runnable,
             continuation,
+            yielder,
             clock,
             waiter: None,
             waker,
