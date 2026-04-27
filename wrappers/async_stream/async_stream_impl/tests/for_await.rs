b4827f89dfd671308d84db763d7b48642f0182ce
Sarek Høverstad Skotåm <44001885+sarsko@users.noreply.github.com>
2026-04-27 12:20:21 -0700

    Update for_await.rs

--- wrappers/async_stream/async_stream_impl/tests/for_await.rs
+++ wrappers/async_stream/async_stream_impl/tests/for_await.rs
@@ -4,22 +4,20 @@ use futures_util::stream::StreamExt;
 
 #[tokio::test]
 async fn test() {
-    {
-        let s = stream! {
-            yield "hello";
-            yield "world";
-        };
+    let s = stream! {
+        yield "hello";
+        yield "world";
+    };
 
-        let s = stream! {
-            for await x in s {
-                yield x.to_owned() + "!";
-            }
-        };
+    let s = stream! {
+        for await x in s {
+            yield x.to_owned() + "!";
+        }
+    };
 
-        let values: Vec<_> = s.collect().await;
+    let values: Vec<_> = s.collect().await;
 
-        assert_eq!(2, values.len());
-        assert_eq!("hello!", values[0]);
-        assert_eq!("world!", values[1]);
-    }
+    assert_eq!(2, values.len());
+    assert_eq!("hello!", values[0]);
+    assert_eq!("world!", values[1]);
 }
